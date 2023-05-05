24d7ce0a29c65923be7c55a61aa4bea08a10b0f2
93amr.kakashi <93amr.kakashi@gmail.com>
2023-05-05 14:20:23 +0200

    edit

--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -37,7 +37,7 @@ import word from "../pics/word.svg";
 import pug from "../pics/pug.svg";
 import vite from "../pics/vite.svg";
 import pwa from "../pics/pwa.svg";
-import graph from "../pics/graoh.svg";
+import graph from "../pics/graph.svg";
 import post from "../pics/postman.svg";
 import Image from "next/image";
 import { divVarient, roboAnimation } from "@/utils/animations";
